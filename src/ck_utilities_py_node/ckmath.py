027e6d4785e8ccf024dc0668d1df71845fd96220
Tom <TSLourenco@WPI.edu>
2023-01-15 15:26:21 -0500

    Converted more CKMath to python.

--- src/ck_utilities_py_node/ckmath.py
+++ src/ck_utilities_py_node/ckmath.py
@@ -1,7 +1,10 @@
 import numpy as np
+import math
+
 
 def handleDeadband(val, deadband):
     return val if abs(val) > abs(deadband) else 0
+
 
 def normalizeWithDeadband(val, deadband):
     val = handleDeadband(val, deadband)
@@ -9,5 +12,30 @@ def normalizeWithDeadband(val, deadband):
         val = np.sign(val) * ((abs(val) - deadband) / (1.0 - deadband))
     return val
 
+
 def within(value1, value2, threshold) -> bool:
     return True if abs(value1 - value2) < abs(threshold) else False
+
+
+def hypotenuse(x, y):
+    return np.sqrt((x*x)+(y*y))
+
+
+def wrapMax(x, max):
+    return np.fmod(max + np.fmod(x, max), max)
+
+
+def wrapMinMax(x, min, max):
+    return min + wrapMax(x - min, max - min)
+
+
+def normalize_to_2_pi(value):
+    return wrapMinMax(value, 0, (2.0 * math.pi))
+
+
+def polar_angle_rad(x, y):
+    return normalize_to_2_pi(np.arctan2(y, x))
+
+def limit(v, minVal, maxVal):
+    return min(maxVal, max(minVal, v))
+        
