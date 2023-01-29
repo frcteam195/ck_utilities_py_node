f9031b57961e1119995e77ec0c1eccb184bd3da2
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-01-29 06:46:37 +0000

    Improve solenoid and add named config ability

--- src/ck_utilities_py_node/solenoid.py
+++ src/ck_utilities_py_node/solenoid.py
@@ -7,6 +7,27 @@ from threading import Thread, Lock
 from ck_ros_base_msgs_node.msg import Solenoid_Control
 from ck_ros_base_msgs_node.msg import Solenoid as SolenoidROS
 from enum import Enum
+from typing import TypeVar, Generic
+
+ValueClass = TypeVar('ValueClass')
+class TimeDelayedValue(Generic[ValueClass]):
+    def __init__(self, transition_delay_sec : float, default_value : ValueClass):
+        self.__curr_value : ValueClass = default_value
+        self.__next_value : ValueClass = default_value
+        self.__delay : float = transition_delay_sec
+        self.__transition_time = -1
+
+    def set(self, value : ValueClass):
+        if self.__next_value != value:
+            self.__next_value = value
+            self.__transition_time = rospy.get_time()
+
+    def get(self) -> ValueClass:
+        if self.__curr_value != self.__next_value:
+            if self.__transition_time != -1 and (rospy.get_time() - self.__transition_time) > self.__delay:
+                self.__curr_value = self.__next_value
+                self.__transition_time = -1
+        return self.__curr_value
 
 class SolenoidType(Enum):
     SINGLE = 0
@@ -74,12 +95,39 @@ class Solenoid:
     manager : SolenoidManager = None
     mutex = Lock()
 
-    def __init__(self, id : int, type : SolenoidType):
+    def __init__(self, *args):
         self.__solenoidControl : SolenoidControl = SolenoidControl()
-        self.__solenoidControl.id = id
-        self.__solenoidControl.type = type
+
+        if isinstance(args[0], int) and isinstance(args[1], SolenoidType):
+            self.__solenoidControl.id = args[0]
+            self.__solenoidControl.type = args[1]
+            self.__delay = 0.150
+        elif isinstance(args[0], str) and isinstance(args[1], SolenoidType):
+            if not rospy.has_param(rospy.get_name() + "/" + args[0] + "_solenoid_id"):
+                raise Exception ("Solenoid: " + args[0] + "_id is not set!")
+            self.__solenoidControl.id = rospy.get_param(rospy.get_name() + "/" + args[0] + "_solenoid_id")
+            self.__solenoidControl.type = args[1]
+            self.__load_solenoid_config(args[0])
+        else:
+            raise Exception ("Solenoid: " + args[0] + "_id invalid constructor!")
+
         self.__solenoidControl.output = SolenoidState.OFF
+        self.__stored_value = TimeDelayedValue[SolenoidState](self.__delay, SolenoidState.OFF)
         self.__spawn_solenoid_manager()
+
+    def __ros_solenoid_config_validation(self, solenoid_string):
+        config_strings = {
+            "_solenoid_id",
+            "_solenoid_actuation_delay"
+        }
+
+        for config_string in config_strings:
+            if not rospy.has_param("/" + rospy.get_name() + "/" + solenoid_string+config_string):
+                raise Exception("Solenoid: " + solenoid_string + " is missing config definition for: " + config_string)
+
+    def __load_solenoid_config(self, solenoid_string):
+        self.__ros_solenoid_config_validation(solenoid_string)
+        self.__delay = rospy.get_param(rospy.get_name() + "/" + solenoid_string + "_solenoid_actuation_delay")
 
     @classmethod
     def __spawn_solenoid_manager(cls):
@@ -93,4 +141,11 @@ class Solenoid:
 
     def set(self, output : SolenoidState):
         self.__solenoidControl.output = output
+        self.__stored_value.set(output)
         self.__update()
+    
+    def get(self) -> SolenoidState:
+        return self.__stored_value.get()
+
+    def get_id(self) -> int:
+        return self.__solenoidControl.id
