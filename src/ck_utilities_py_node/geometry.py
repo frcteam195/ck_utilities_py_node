af296af9db2e02cdba148a2320376b52135cca25
Michael Todd <eltodd@gmail.com>
2023-01-30 20:14:20 +0000

    Cont implementation

--- src/ck_utilities_py_node/geometry.py
+++ src/ck_utilities_py_node/geometry.py
@@ -1,55 +1,103 @@
 import numpy
 from dataclasses import dataclass
+from geometry_msgs.msg import *
+from tf.transformations import *
+from functools import singledispatchmethod
 
 class Translation:
     def __init__(self):
         self.__translation = numpy.zeros(3)
-    def x(self) -> float:
+
+    @property
+    def x(self):
         return self.__translation[0]
-    def x(self, value : float):
+    @x.setter
+    def x(self, value):
         self.__translation[0] = value
-    def y(self) -> float:
+
+    @property
+    def y(self):
         return self.__translation[1]
-    def y(self, value : float):
+    @y.setter
+    def y(self, value):
         self.__translation[1] = value
-    def z(self) -> float:
+
+    @property
+    def z(self):
         return self.__translation[2]
-    def z(self, value : float):
+    @z.setter
+    def z(self, value):
         self.__translation[2] = value
 
 class Rotation:
-    def __init__(self):
+
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__rotation = numpy.zeros(3)
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Quaternion.Quaternion):
+            self.__init_from_quaternion(input_type)
+            return
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by Rotation constructor")
+
+    def __init_from_quaternion(self, input_quaternion : geometry_msgs.msg._Quaternion.Quaternion):
         self.__rotation = numpy.zeros(3)
+        quat = ([input_quaternion.x, input_quaternion.y, input_quaternion.z, input_quaternion.w])
+        eulers = euler_from_quaternion(quat)
+        self.roll = eulers[0]
+        self.yaw = eulers[1]
+        self.pitch = eulers[2]
+
+    @property
     def roll(self) -> float:
         return self.__rotation[0]
+    @roll.setter
     def roll(self, value : float):
         self.__rotation[0] = value
+
+    @property
     def pitch(self) -> float:
         return self.__rotation[1]
+    @pitch.setter
     def pitch(self, value : float):
         self.__rotation[1] = value
+
+    @property
     def yaw(self) -> float:
         return self.__rotation[2]
+    @yaw.setter
     def yaw(self, value : float):
         self.__rotation[2] = value
 
 class RotationQuaternion:
     def __init__(self):
-        self.__rotation = numpy.zeros(4,4)
+        self.__rotation = numpy.zeros((4,4))
+    
+    @property
     def x(self) -> float:
         return self.__rotation[0,0]
+    @x.getter
     def x(self, value : float):
         self.__rotation[0,0] = value
+    
+    @property
     def y(self) -> float:
         return self.__rotation[1,1]
+    @y.getter
     def y(self, value : float):
         self.__rotation[1,1] = value
+    
+    @property
     def z(self) -> float:
         return self.__rotation[2,2]
+    @z.getter
     def z(self, value : float):
         self.__rotation[2,2] = value
+    
+    @property
     def w(self) -> float:
         return self.__rotation[3,3]
+    @w.getter
     def w(self, value : float):
         self.__rotation[3,3] = value
 
