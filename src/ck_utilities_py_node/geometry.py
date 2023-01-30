612487eae43a70c80c0a2437a2a0fb9dba083adc
Michael Todd <eltodd@gmail.com>
2023-01-30 18:42:55 +0000

    Initial commit

--- src/ck_utilities_py_node/geometry.py
+++ src/ck_utilities_py_node/geometry.py
@@ -1,33 +1,67 @@
-import numpy as np
+import numpy
 from dataclasses import dataclass
 
-@dataclass
 class Translation:
-    x : float = 0
-    y : float = 0
-    z : float = 0
+    def __init__(self):
+        self.__translation = numpy.zeros(3)
+    def x(self) -> float:
+        return self.__translation[0]
+    def x(self, value : float):
+        self.__translation[0] = value
+    def y(self) -> float:
+        return self.__translation[1]
+    def y(self, value : float):
+        self.__translation[1] = value
+    def z(self) -> float:
+        return self.__translation[2]
+    def z(self, value : float):
+        self.__translation[2] = value
 
-@dataclass
 class Rotation:
-    yaw : float = 0
-    pitch : float = 0
-    roll : float = 0
+    def __init__(self):
+        self.__rotation = numpy.zeros(3)
+    def roll(self) -> float:
+        return self.__rotation[0]
+    def roll(self, value : float):
+        self.__rotation[0] = value
+    def pitch(self) -> float:
+        return self.__rotation[1]
+    def pitch(self, value : float):
+        self.__rotation[1] = value
+    def yaw(self) -> float:
+        return self.__rotation[2]
+    def yaw(self, value : float):
+        self.__rotation[2] = value
 
-@dataclass
 class RotationQuaternion:
-    x : float = 0
-    y : float = 0
-    z : float = 0
-    w : float = 1
+    def __init__(self):
+        self.__rotation = numpy.zeros(4,4)
+    def x(self) -> float:
+        return self.__rotation[0,0]
+    def x(self, value : float):
+        self.__rotation[0,0] = value
+    def y(self) -> float:
+        return self.__rotation[1,1]
+    def y(self, value : float):
+        self.__rotation[1,1] = value
+    def z(self) -> float:
+        return self.__rotation[2,2]
+    def z(self, value : float):
+        self.__rotation[2,2] = value
+    def w(self) -> float:
+        return self.__rotation[3,3]
+    def w(self, value : float):
+        self.__rotation[3,3] = value
 
 class Pose:
-    position : Translation
-    orientation : Rotation
+    def __init__(self):
+        self.position : Translation = Translation()
+        self.orientation : Rotation = Rotation()
 
 class Transform:
     def __init__(self):
-        self.linear = Translation()
-        self.angular = Rotation()
+        self.linear : Translation = Translation()
+        self.angular : Rotation = Rotation()
 
 class Scale:
     def __init__(self, x : float, y : float, z : float):
@@ -35,7 +69,6 @@ class Scale:
         self.y : float = y
         self.z : float = z
 
-@dataclass
 class Twist:
     linear : Translation
     angular : Rotation
