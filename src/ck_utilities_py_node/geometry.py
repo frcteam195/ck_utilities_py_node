e60322d07c72cc70eb055dea507e665a889b99f9
Michael Todd <eltodd@gmail.com>
2023-01-31 00:04:38 +0000

    Addtl geometry updates

--- src/ck_utilities_py_node/geometry.py
+++ src/ck_utilities_py_node/geometry.py
@@ -3,8 +3,29 @@ from geometry_msgs.msg import *
 from tf.transformations import *
 
 class Translation:
-    def __init__(self):
+
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__translation = numpy.zeros(3)
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Vector3.Vector3):
+            self.__init_from_translation(input_type)
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Point.Point)
+            self.__init_from_point(input_type)
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by Translation constructor")
+
+    def __init_from_translation(self, input_translation : geometry_msgs.msg._Vector3.Vector3):
         self.__translation = numpy.zeros(3)
+        self.x = input_translation.x
+        self.y = input_translation.y
+        self.z = input_translation.z
+
+    def __init_from_point(self, input_point : geometry_msgs.msg._Point.Point)
+        self.__translation = numpy.zeros(3)
+        self.x = input_point.x
+        self.y = input_point.y
+        self.z = input_point.z
 
     @property
     def x(self):
@@ -68,30 +89,44 @@ class Rotation:
         self.__rotation[2] = value
 
 class RotationQuaternion:
-    def __init__(self):
-        self.__rotation = numpy.zeros((4,4))
-    
+
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__rotation = numpy.zeros(4,4)
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Quaternion.Quaternion):
+            self.__init_from_quaternion(input_type)
+            return
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
+
+    def __init_from_quaternion(self, input_quaternion : geometry_msgs.msg._Quaternion.Quaternion):
+        self.__rotation = numpy.zeros(4,4)
+        self.x = input_quaternion.x
+        self.y = input_quaternion.y
+        self.z = input_quaternion.z
+        self.w = input_quaternion.w
+
     @property
     def x(self) -> float:
         return self.__rotation[0,0]
     @x.getter
     def x(self, value : float):
         self.__rotation[0,0] = value
-    
+
     @property
     def y(self) -> float:
         return self.__rotation[1,1]
     @y.getter
     def y(self, value : float):
         self.__rotation[1,1] = value
-    
+
     @property
     def z(self) -> float:
         return self.__rotation[2,2]
     @z.getter
     def z(self, value : float):
         self.__rotation[2,2] = value
-    
+
     @property
     def w(self) -> float:
         return self.__rotation[3,3]
@@ -100,22 +135,96 @@ class RotationQuaternion:
         self.__rotation[3,3] = value
 
 class Pose:
-    def __init__(self):
-        self.position : Translation = Translation()
-        self.orientation : Rotation = Rotation()
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__position : Translation = Translation()
+            self.__orientation : Rotation = Rotation()
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Pose.Pose):
+            self.__init_from_pose(input_type)
+            return
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
+
+    def __init_from_pose(self, input_pose : geometry_msgs.msg._Pose.Pose):
+        self.__position = Translation(input_pose.position)
+        self.__orientation = Rotation(input_pose.orientation)
+
+    @property
+    def position(self) -> Translation:
+        return self.__position
+    @position.getter
+    def position(self, value : Translation):
+        self.__position = value
+
+    @property
+    def orientation(self) -> Rotation:
+        return self.__orientation
+    @orientation.getter
+    def position(self, value : Rotation):
+        self.__orientation = value
 
 class Transform:
-    def __init__(self):
-        self.linear : Translation = Translation()
-        self.angular : Rotation = Rotation()
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__linear : Translation = Translation()
+            self.__angular : Rotation = Rotation()
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Transform.Transform):
+            self.__init_from_pose(input_type)
+            return
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
+
+    def __init_from_pose(self, input_transform : geometry_msgs.msg._Transform.Transform):
+        self.__linear = Translation(input_transform.translation)
+        self.__angular = Rotation(input_transform.rotation)
+
+    @property
+    def linear(self) -> Translation:
+        return self.__linear
+    @linear.getter
+    def linear(self, value : Translation):
+        self.__linear = value
+
+    @property
+    def angular(self) -> Rotation:
+        return self.__angular
+    @angular.getter
+    def angular(self, value : Rotation):
+        self.__angular = value
+
+
+class Twist:
+    def __init__(self, input_type = None):
+        if input_type is None:
+            self.__linear : Translation = Translation()
+            self.__angular : Rotation = Rotation()
+            return
+        elif isinstance(input_type, geometry_msgs.msg._Transform.Transform):
+            self.__init_from_pose(input_type)
+            return
+        raise ValueError("Type " + str(type(input_type)) + "is not supported by RotationQuaternion constructor")
+
+    def __init_from_pose(self, input_transform : geometry_msgs.msg._Transform.Transform):
+        self.__linear = Translation(input_transform.translation)
+        self.__angular = Rotation(input_transform.rotation)
+
+    @property
+    def linear(self) -> Translation:
+        return self.__linear
+    @linear.getter
+    def linear(self, value : Translation):
+        self.__linear = value
+
+    @property
+    def angular(self) -> Rotation:
+        return self.__angular
+    @angular.getter
+    def angular(self, value : Rotation):
+        self.__angular = value
+
 
 class Scale:
     def __init__(self, x : float, y : float, z : float):
         self.x : float = x
         self.y : float = y
         self.z : float = z
-
-class Twist:
-    linear : Translation
-    angular : Rotation
-
