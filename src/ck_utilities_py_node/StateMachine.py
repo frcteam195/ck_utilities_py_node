ae2261558e1d90ca082e1d9e8b8968566a71a63b
Michael Todd <eltodd@gmail.com>
2023-02-11 21:05:02 +0000

    Add logging support to state machine

--- src/ck_utilities_py_node/StateMachine.py
+++ src/ck_utilities_py_node/StateMachine.py
@@ -1,4 +1,8 @@
 from abc import ABC, abstractmethod
+from queue import Queue
+from ck_ros_base_msgs_node.msg import StateMachineLog
+import rospy
+
 
 class StateMachine(ABC):
 
@@ -28,17 +32,40 @@ class StateMachine(ABC):
         self.states = states
 
         self.states[self.state].entry(self)
+        self.transition_history = Queue(10)
+
+        self.log_count = 0
+        self.transition_history.put(str(self.log_count) + ": Init: " + str(self.state))
+        self.log_count += 1
+        self.log_publisher = rospy.Publisher(name="/state_machines/" + str(self.__class__.__name__),
+                                             data_class=StateMachineLog,
+                                             queue_size=100,
+                                             tcp_nodelay=False)
 
     def get_current_state(self) -> str:
         return self.state
+
+    def log_data(self):
+        log_string = ""
+        for i in self.transition_history.queue:
+            log_string += i + "\n"
+        log_msg = StateMachineLog()
+        log_msg.current_state = str(self.state)
+        log_msg.current_state_int = self.state.value
+        log_msg.transition_history = log_string
+        self.log_publisher.publish(log_msg)
 
     def step(self):
         initial_state = self.state
         self.states[self.state].step(self)
         self.state = self.states[self.state].transition(self)
         while self.state is not initial_state:
+            if self.transition_history.full():
+                self.transition_history.get()
+            self.transition_history.put(str(self.log_count) + ": TRANSITION: " + str(initial_state) + " -> " + str(self.state))
+            self.log_count += 1
             self.states[self.state].entry(self)
             initial_state = self.state
             self.states[self.state].step(self)
             self.state = self.states[self.state].transition(self)
-
+        self.log_data()
